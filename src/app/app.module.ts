e465d4fe6c4bbd2ccfc92fb6d35a58507780a608
gnappo1 <gnappo.1@live.it>
2018-05-21 22:32:31 -0700

    created server component manually and with cli commands.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+//TypeScript wants us to import components in order for them to be used by Angular
+import { ServerComponent } from './server/server.component'
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
+//angular decorator where you register new components, add new modules to make this module leaner (imports).
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
